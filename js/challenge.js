6233e3aa2366922a22ba4d0e461dbf22dca454d0
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-20 18:28:38 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -15,9 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let commentList = document.querySelector("#list")
     
   
-let pause_id = document.getElementById("pause")
-let minus_id = document.getElementById("-")
-let plus_id = document.getElementById("+")
+
+
+
 let heart_id = document.getElementById("<3")
 let list_id = document.getElementById("list")
 let comment_id = document.getElementsByTagName("form")[0]
