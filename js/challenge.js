8d3a9bbe5a9490cc2b590cfd6377f2244c28deb6
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-20 18:29:14 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let submitBtn = document.querySelector("#submit")
     let commentList = document.querySelector("#list")
     
-    
+
     
     
     
