b62fbe73b15b204ef41c59ae706236038c62fc98
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-20 18:46:22 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let love = document.getElementById("<3")
     let list_id = document.getElementById("list")
     let comment = document.getElementsByTagName("form")[0]
-    let likes = document.querySelector(".likes")
     let submitBtn = document.querySelector("#submit")
  
     minus.addEventListener("click", function(e){
